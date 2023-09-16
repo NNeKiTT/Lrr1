28f27f55e095f49b572b92f7ffa660cb61e0b680
NNeKiTT <nikita.gaydin.nmsk@gmail.com>
2023-09-16 19:23:27 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Pz-20-1/9, Gaydin Nikita");
+        System.out.println(LocalDate.now());
     }
 }
